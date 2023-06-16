2937a9721581d09645b7883a47a7443ccf4a1486
BarkerEmily <emilybarker@byui.edu>
2023-06-16 10:15:43 -0600

    router.get

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -10,32 +10,38 @@ const utilities = require("../utilities")
 // unit 4 server-side data validation activity
 const regValidate = require('../utilities/account-validation')
 
-
 /* ****************************************
 *  Deliver login view unit 4 activity
 * *************************************** */
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
-// register post
-router.post('/register', utilities.handleErrors(accountController.buildRegister))
+// register get
+router.get('/register', utilities.handleErrors(accountController.buildRegister))
+// account-mangaement get
+router.get('/account-management', utilities.handleErrors(accountController.buildAccountManagement))
 
 // Process the registration data
 router.post(
     "/register",
     regValidate.registationRules(),
     regValidate.checkRegData,
+    regValidate.registerAccount,
     utilities.handleErrors(accountController.buildRegister)
   )
 
 // Process the login attempt
 router.post(
   "/login",
-  (req, res) => {
-    res.status(200).send('login process')
-    regValidate.registationRules(),
+    regValidate.checkLogin(),
     regValidate.checkRegData,
+    regValidate.accountLogin,
     utilities.handleErrors(accountController.buildLogin)
-  }
 )
 
+// Process the account-management
+router.post(
+  "/account-management",
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.buildAccountManagement)
+)
 
 module.exports = router
